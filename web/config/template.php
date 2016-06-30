7b0e1ac2d2119883cc44742dc4de427e134396fb
Alexon Balangue <moi.sites@alexonbalangue.me>
2016-06-30 07:48:35 +0200

    news files on directory web

---
+++ web/config/template.php
@@ -0,0 +1,9 @@
+<?php
+define('__BOOSTRAPVERSION__', '3.3.6');
+define('__FONTAWESOMEVERSION__', '4.6.3');
+define('__JQUERYVERSION__', '1.12.4');
+
+//Default domaine name (=ip dedicated/shared)
+$my_domainname = 'www.exemple.gtld'
+
+?>
---
+++ web/pages/error/403.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+	<div class="Full">
+		<h1 class="text-center"><i class="fa fa-warning fa-5x fa-spin"></i><h1>
+		<h2 class="tex-left"><?php echo $title; ?></h2>
+		<p class="tex-right"><?php echo $desc; ?></p>
+	</div>
+</div>
