7bb31ef52132dc98d16036ba03129a543cc07d06
Alexon Balangue <moi.sites@alexonbalangue.me>
2016-07-02 11:34:57 +0200

    add/edit sources code

--- web/pages/error/504.php
+++ web/pages/error/504.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="container">
 	<div class="Full">
 		<h1 class="text-center"><i class="fa fa-warning fa-5x fa-spin"></i><h1>
 		<h2 class="text-left"><?php echo $title; ?></h2>
