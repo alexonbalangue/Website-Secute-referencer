07559eed138f53a00c275747ee314fb0d72968b6
Alexon Balangue <moi.sites@alexonbalangue.me>
2016-07-02 11:35:51 +0200

    translate original [en] other not yet

--- web/language/fr.translate.php
+++ web/language/fr.translate.php
@@ -2,19 +2,41 @@
 
 $lang = array();
 
-$lang['error_403_title'] = '403 Forbidde'; 
-$lang['error_403_desc'] = 'The server has refused to fulfill your request.'; 
-$lang['error_404_title'] = '404 Not Found'; 
-$lang['error_404_desc'] = 'The document/file requested was not found on this server.'; 
-$lang['error_405_title'] = '405 Method Not Allowed'; 
+$lang['error_400_title'] = '400 BAD REQUEST'; 
+$lang['error_400_desc'] = 'Your browser (or proxy) sent a request that this server could not understand.'; 
+$lang['error_401_title'] = '401 UNAUTHORIZED'; 
+$lang['error_401_desc'] = 'The server does not support the media type transmitted in the request.'; 
+$lang['error_403_title'] = '403 FORBIDDEN'; 
+$lang['error_403_desc'] = 'You don\'t have permission to access the requested directory. There is either no index document or the directory is read-protected.'; 
+$lang['error_404_title'] = '404 NOT_FOUND'; 
+$lang['error_404_desc'] = 'The requested URL was not found on this server.'; 
+$lang['error_405_title'] = '405 METHOD NOT ALLOWED'; 
 $lang['error_405_desc'] = 'The method specified in the Request-Line is not allowed for the specified resource.'; 
-$lang['error_408_title'] = '408 Request Timeout'; 
-$lang['error_408_desc'] = 'Your browser failed to send a request in the time allowed by the server.'; 
-$lang['error_500_title'] = '500 Internal Server Error'; 
-$lang['error_500_desc'] = 'The request was unsuccessful due to an unexpected condition encountered by the server.'; 
-$lang['error_502_title'] = '502 Bad Gateway'; 
-$lang['error_502_desc'] = 'The server received an invalid response from the upstream server while trying to fulfill the request.'; 
-$lang['error_504_title'] = '504 Gateway Timeout'; 
+$lang['error_408_title'] = '408 REQUEST TIME OUT'; 
+$lang['error_408_desc'] = 'The server closed the network connection because the browser didn\'t finish the request within the specified time.'; 
+$lang['error_410_title'] = '410 GONE'; 
+$lang['error_410_desc'] = 'The requested URL is no longer available on this server and there is no forwarding address.'; 
+$lang['error_411_title'] = '411 LENGTH REQUIRED'; 
+$lang['error_411_desc'] = 'A request with the method requires a valid <code>Content-Length</code> header.'; 
+$lang['error_412_title'] = '412 PRECONDITION FAILED'; 
+$lang['error_412_desc'] = 'The precondition on the request for the URL failed positive evaluation.'; 
+$lang['error_413_title'] = '413 REQUEST ENTITY TOO LARGE'; 
+$lang['error_413_desc'] = ''; 
+$lang['error_414_title'] = '414 REQUEST URI TOO LARGE'; 
+$lang['error_414_desc'] = 'The length of the requested URL exceeds the capacity limit for this server. The request cannot be processed.'; 
+$lang['error_415_title'] = '415 UNSUPPORTED_MEDIA_TYPE'; 
+$lang['error_415_desc'] = 'This server could not verify that you are authorized to access the URL. You either supplied the wrong credentials (e.g., bad password), or your browser doesn\'t understand how to supply the credentials required.'; 
+$lang['error_500_title'] = '500 INTERNAL SERVER ERROR'; 
+$lang['error_500_desc'] = 'The server encountered an internal error and was unable to complete your request.'; 
+$lang['error_501_title'] = '501 NOT IMPLEMENTED'; 
+$lang['error_501_desc'] = 'The server does not support the action requested by the browser.'; 
+$lang['error_502_title'] = '502 BAD_GATEWAY'; 
+$lang['error_502_desc'] = 'The proxy server received an invalid response from an upstream server.'; 
+$lang['error_503_title'] = '503 SERVICE UNAVAILABLE'; 
+$lang['error_503_desc'] = 'The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later. '; 
+$lang['error_504_title'] = '504 GATEWAY TIMEOUT'; 
 $lang['error_504_desc'] = 'The upstream server failed to send a request in the time allowed by the server.'; 
+$lang['error_506_title'] = '506 VARIANT ALSO VARIES'; 
+$lang['error_506_desc'] = 'A variant for the requested entity is itself a negotiable resource. Access not possible.'; 
 
 ?>
